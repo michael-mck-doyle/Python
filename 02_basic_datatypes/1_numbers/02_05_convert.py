bcf280512d11695e7d8d54d282f2d245d68dd5ea
Michael Doyle <meyekkul@gmail.com>
2020-03-19 20:46:38 +0800

    updating datatype labs

--- 02_basic_datatypes/1_numbers/02_05_convert.py
+++ 02_basic_datatypes/1_numbers/02_05_convert.py
@@ -10,3 +10,20 @@ Demonstrate how to:
     Take note of what information is lost when some conversions take place.
 
 '''
+
+x = 10
+x = 10/3
+print(x)
+x = int(x)
+print(x)
+x = 10 /4
+print(x)
+x = 10//4
+print(x)
+
+x = int(input("Please enter a number: "))
+y = int(input("Please enter another number: "))
+
+x = x * y
+print("multiplying these numbers gives you: " + str(x))
+
