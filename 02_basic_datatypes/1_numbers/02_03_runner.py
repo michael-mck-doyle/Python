aa2d7baf5e583a71d37c7511ef3214ae5569ee2c
Michael Doyle <meyekkul@gmail.com>
2020-03-19 14:45:48 +0800

    updating datatype labs

--- 02_basic_datatypes/1_numbers/02_03_runner.py
+++ 02_basic_datatypes/1_numbers/02_03_runner.py
@@ -4,3 +4,20 @@ If a runner runs 10 miles in 30 minutes and 30 seconds,
 What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
 
 '''
+
+distance_miles = 10
+distance_kilometres = distance_miles*1.6
+minutes = 30
+seconds = 30
+time_hours = ((minutes*60)+seconds)/3600
+
+print("running time is " + format(time_hours, '.2f') + " hours")
+
+speed = distance_kilometres / time_hours
+
+print("speed in km/hr = " +format(speed, '.2f'))
+
+
+
+
+
