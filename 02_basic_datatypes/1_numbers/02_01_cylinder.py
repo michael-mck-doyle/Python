ac125629a385083c1c55fc352ba90f54077a81ed
Michael Doyle <meyekkul@gmail.com>
2020-03-19 14:10:47 +0800

    updating datatype labs

--- 02_basic_datatypes/1_numbers/02_01_cylinder.py
+++ 02_basic_datatypes/1_numbers/02_01_cylinder.py
@@ -4,3 +4,31 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+
+#radius = 3.14
+#height = 5
+pi = 3.1416
+
+radius = int(input("Please enter a radius in centimeters: "))
+height = int(input("Please enter a height in centimeters: "))
+cylinder_volume = (radius**2) * height * pi
+
+print ("cylinder_volume = " + str(cylinder_volume))
+
+
+surface_area = (2 * pi * radius**2) + ((2 * pi * radius) * height)
+
+print ("surface_area = " + str(surface_area))
+
+
+
+
+
+
+
+
+
+
+
+
+
