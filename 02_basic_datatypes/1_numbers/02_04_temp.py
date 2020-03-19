8306e60a083f6abe278166915930dfb2cb5e42f0
Michael Doyle <meyekkul@gmail.com>
2020-03-19 20:08:28 +0800

    updating datatype labs

--- 02_basic_datatypes/1_numbers/02_04_temp.py
+++ 02_basic_datatypes/1_numbers/02_04_temp.py
@@ -10,3 +10,15 @@ Output should read like - "81.32 degrees fahrenheit = 27.4 degrees celsius"
 
 
 '''
+
+temp_Farh = int(input("Please enter a temperature in Fahrenheit: "))
+
+temp_C = (temp_Farh - 32) * (5 / 9)
+temp_C = format(temp_C, '.2f')
+#print(temp_C)
+
+
+print (str(temp_Farh) + " degrees fahrenheit = " + str(temp_C) + " degrees Celsius")
+
+
+
