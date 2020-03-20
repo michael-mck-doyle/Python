314eb9325faadf9258054d1f5f42e9e433f78238
Michael Doyle <meyekkul@gmail.com>
2020-03-20 17:22:12 +0800

    updating datatype labs

--- 02_basic_datatypes/1_numbers/02_06_invest.py
+++ 02_basic_datatypes/1_numbers/02_06_invest.py
@@ -7,3 +7,23 @@ Take in the following three values from the user:
 Print the future values to the console.
 
 '''
+
+
+
+P = int(input("Please enter the amount of dollars you would like to invest: "))
+r = float(input("Please enter the interest rate in decimal: "))
+t = int(input("Please enter the number of years you would like to invest for: "))
+n = int(input("Please enter the number of times interest is compounded each year e.g. monthly (12), quarterly (4) or yearly (1): "))
+
+A = P*(1+r/n)**(n*t)
+A = format(A, '.2f')
+print("After " + str(t) + " years " + str(P) + " dollars will be worth " + str(A))
+
+
+
+# A = the future value of the investment
+# P = the principal investment amount
+# r = the interest rate (decimal)
+# n = the number of times that interest is compounded per period
+# t = the number of periods the money is invested for
+
