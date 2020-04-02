f9d07c5e5477e6204a904e6a2ab3a0a8cc5b26de
Michael Doyle <meyekkul@gmail.com>
2020-04-02 13:54:25 +0800

    updating basic datatypes labs

--- 02_basic_datatypes/2_strings/02_11_slicing.py
+++ 02_basic_datatypes/2_strings/02_11_slicing.py
@@ -1,9 +1,30 @@
 '''
 
-Using string slicing, take in the user's name and print out their name translated to pig latin.
-For the purpose of this program, we will say that any word or name can be
-translated to pig latin by moving the first letter to the end, followed by "ay".
+Using string slicing, take in the user's name and print out their name translated,
+moving the first letter to the end, followed by "ay".
 
 For example: ryan -> yanray, caden -> adencay
 
 '''
+n = []
+s = 0
+while s < 3:
+    name = (input("Please enter a name: "))
+    x = name[0]
+    y = name[1:]
+    z = "ay"
+    print(x, y, z)
+    new_name = y + x + z
+    n.append(new_name)
+    print(new_name)
+    s = s + 1
+
+
+print(n)
+
+
+
+
+
+
+
