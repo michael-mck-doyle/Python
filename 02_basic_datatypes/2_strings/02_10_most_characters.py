bf98c4561ee831d67e3e8c067a56ba46e8aa0f82
Michael Doyle <meyekkul@gmail.com>
2020-04-02 11:29:37 +0800

    updating 02_basic_datatypes labs

--- 02_basic_datatypes/2_strings/02_10_most_characters.py
+++ 02_basic_datatypes/2_strings/02_10_most_characters.py
@@ -11,3 +11,60 @@ CHALLENGE: Can you edit to script to print only the string with the most charact
            into the topic "Conditionals" to solve this challenge.
 
 '''
+
+'''
+Note, the solution below doesn't work if strings of the same length are entered e.g. Sunday, Monday, Friday
+'''
+s = 0
+
+list_string = []
+
+while s < 3:
+    r = (input("Please enter a string of words: "))
+
+    list_string.append(r)
+
+    s = s + 1
+
+list_string.sort(key=len, reverse=True)
+
+for x in list_string:
+    print(len(x), ",", x)
+
+print(list_string[0], " is the longest string entered.")
+
+
+'''
+- the code below are some of the other things i tried to come up with a solution
+- this doesn't work because the max(list_string) below only considers the length
+ of the first word in the string i think, not the total length of the string.  This is interesting because
+ it highlights some of the test cases that can be carried out to check the code works under different conditions
+ 
+
+
+
+max_len = (max(list_string))
+print(max_len, " is the longest string.")
+'''
+
+
+
+
+'''
+use loop
+for x, y in thisdict.items():
+  print(x, y)
+
+#other stuff i tried
+#print("days of the week include: ", list_string)
+#list_dict = {}
+#print(len(r), r)
+    #x = len(r), r
+    #print(x)
+    #list_string.append(r)
+#max_list_dict = max(list_dict, key=list_dict.get)
+#print(max_list_dict)
+
+#for max_len in list_dict:
+   # print (max_len, list_dict[max_len])
+'''
