be7ba57b82649f86e372d83d61588a1d292c3633
Michael Doyle <meyekkul@gmail.com>
2020-04-15 11:18:30 +0800

    updating 03_more_datatypes labs

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -11,3 +11,40 @@ Note: This lab might be challenging! Make sure to discuss it with your mentor
 or chat about it on our forum.
 
 '''
+
+from random import randrange
+x = 0
+my_num_list = []
+next_num_list = []
+
+while x < 20:
+    y = (randrange(1, 100))
+    print(y)
+    my_num_list.append(y)
+    x += 1
+print(my_num_list)
+
+# create individual tuples by slicing the list
+
+while x <= len(my_num_list):
+    z = my_num_list[0:20:10]
+    t = next_num_list.append(z)
+
+    print(my_tuple)
+
+    x += 1
+
+
+
+# add the individual tuples back to new list
+'''
+print(my_num_list)
+
+my_tuple = tuple(my_num_list)
+print(my_tuple)
+
+for i in my_num_list:
+    while x <= len(my_num_list):
+        y = my_num_list.split(' , ')
+        print(y)
+'''
--- 03_more_datatypes/4_dictionaries/03_20_dict_tuples.py
+++ 03_more_datatypes/4_dictionaries/03_20_dict_tuples.py
@@ -11,3 +11,17 @@ NOTE: Check out the Python docs and see whether you can come up with a solution,
 
 '''
 
+# dictionary >>> list >>> tuples
+
+from operator import itemgetter, attrgetter
+
+my_dict = {"emily": 12, "zax": 23, "mia": 13, "jeremiah": 4, "russel": 15}
+my_tuples_list = my_dict.items()
+print(my_tuples_list)
+print()
+# enter either 0, 1, 2...etc...for the key itemgetter...the list will then be sorted based on the entry value
+my_tuples_list = sorted(my_tuples_list, key=itemgetter(0))
+print("my_tuples_list: ", my_tuples_list)
+print()
+second_tuples_list = sorted(my_dict, key=lambda name: name[0])
+print("second_tuples_list: ", second_tuples_list)
