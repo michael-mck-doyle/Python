1a5f1a29fe5a99d80e76a4a1ee26d9fedc749d6c
Michael Doyle <meyekkul@gmail.com>
2020-04-27 09:49:51 +0800

    updating unit testing references

--- 10_testing/test_first.py
+++ 10_testing/test_first.py
@@ -5,6 +5,22 @@ import first
 
 class TestUnit(unittest.TestCase):
     """docstring placeholder"""
+
+    @classmethod
+    def setUpClass(cls):
+        print('Class level hook method for setting up class fixture before running tests in the class.')
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Class level hook method for deconstructing the class fixture after running all tests in the class.")
+
+    def setUp(self):
+        print("Test level hook method for setting up the test fixture before each individual test.")
+
+    def tearDown(self):
+        print("Test level hook method for deconstructing the test fixture after each individual test.")
+        pass
+
     def test_add(self):
         self.assertEqual(first.addition(9, 8), 17)
 
