1e01b3144f2092d0799148475add129b31b805d6
Michael Doyle <meyekkul@gmail.com>
2020-04-27 13:09:32 +0800

    updating Functions unit tests

---
+++ 06_functions/tasks_06_01.py
@@ -0,0 +1,61 @@
+'''
+
+Write a script that completes the following tasks.
+
+'''
+
+# define a function that determines whether the number is divisible by 4 OR 7 and returns a boolean
+
+#a = input("Enter any number: ")
+
+
+def mod4_or_7(a):
+    x = int(a) % 4
+    y = int(a) % 7
+
+    if x == 0 or y == 0:
+        return True
+    else:
+        return False
+
+
+div = mod4_or_7(98)
+print(div)
+
+
+# define a function that determines whether a number is divisible by both 4 AND 7 and returns a boolean
+
+#b = input("Enter any number: ")
+
+
+def mod4_and_7(b):
+
+    x = int(b) % 4
+    y = int(b) % 7
+
+    if x == 0 and y == 0:
+        return True
+    else:
+        return False
+
+
+div = mod4_and_7(102)
+print(div)
+
+
+# call your functions, passing in the user input as the arguments, and set their output equal to new variables
+# print your new variables to display the results
+
+
+"""
+def num_input():
+    num = input("Enter a number between 1 an 1,000,000: ")
+    y = int(num)
+    if y in range(1, 1000000):
+        return y
+    else:
+        return "The number you entered is not within the required range"
+
+x = num_input()
+print(x, "is a number between 1 and 1,000,000")
+"""
---
+++ 06_functions/test_tasks.py
@@ -0,0 +1,18 @@
+import unittest
+import tasks_06_01
+
+
+class TestTasks(unittest.TestCase):
+    """ docstrings"""
+    def test_div4or7(self):
+        self.assertEqual(tasks_06_01.mod4_or_7(7), True)
+        self.assertEqual(tasks_06_01.mod4_or_7(3), False)
+
+    def test_dic4and7(self):
+        self.assertEqual(tasks_06_01.mod4_and_7(28), True)
+        self.assertEqual(tasks_06_01.mod4_and_7(14), False)
+        self.assertEqual(tasks_06_01.mod4_and_7(5), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
