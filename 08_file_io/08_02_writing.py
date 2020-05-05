8b685a42b943ac3dcbef32d03c30d7a1dd664097
Michael Doyle <meyekkul@gmail.com>
2020-05-05 11:35:32 +0800

    updating file i/o labs

--- 08_file_io/08_02_writing.py
+++ 08_file_io/08_02_writing.py
@@ -3,39 +3,18 @@ Write a script that reads in the contents of words.txt and writes the contents i
 to a new file words_reverse.txt.
 '''
 
-# References
-# 7.2. Reading and Writing Files - https://docs.python.org/3/tutorial/inputoutput.html
+all_words = []
+
+with open('words.txt', 'r') as fin:
+    for word in fin.readlines():
+        #all_words.append(word)
+        all_words.append(word.rstrip("\n"))
+        
+with open('words_reverse.txt', 'w') as f_out:
+    for word in all_words:
+        all_words.reverse()
+    f_out.write(str(all_words))
 
 
-# To write to a file you must first open the file in write mode.  Using "w" will overwrite anything currently within the file.
-fout = open('output.txt', 'w')
-
-# We can then write data to the file "output.txt" using the write() method
-fout.write('Writing to a file for the first time')
-
-# Lastly, it is important to always close the file after opening:
-fout.close()
-
-# using "a" appends new data to the file
-f = open("output.txt", "a")
-f.write(". That wasn't so bad!")
-f.close()
 
 
-# Reading from Files
-# First, we have to open the file in read mode:
-fin = open('input.txt', 'r')
-
-# We can then use one of the many read methods python provides.
-# The read() method reads the entire file and returns the contents as a string. For example:
-contents = fin.read()
-
-# don't forget to close the file
-fin.close()
-
-# using "with"
-with open("output.txt", "r") as fin:
-    for line in fin.readline():
-
-with open('input.txt', 'r') as fin:
-    print(fin.readline())
---
+++ 19_Practice_Folder/19_27_file_IO.py
@@ -0,0 +1,36 @@
+# References
+# 7.2. Reading and Writing Files - https://docs.python.org/3/tutorial/inputoutput.html
+
+
+# To write to a file you must first open the file in write mode.  Using "w" will overwrite anything currently within the file.
+fout = open('output.txt', 'w')
+
+# We can then write data to the file "output.txt" using the write() method
+fout.write('Writing to a file for the first time')
+
+# Lastly, it is important to always close the file after opening:
+fout.close()
+
+# using "a" appends new data to the file
+f = open("output.txt", "a")
+f.write(". That wasn't so bad!")
+f.close()
+
+
+# Reading from Files
+# First, we have to open the file in read mode:
+fin = open('input.txt', 'r')
+
+# We can then use one of the many read methods python provides.
+# The read() method reads the entire file and returns the contents as a string. For example:
+contents = fin.read()
+
+# don't forget to close the file
+fin.close()
+
+# using "with"
+with open("output.txt", "r") as fin:
+    for line in fin.readline():
+
+with open('input.txt', 'r') as fin:
+    print(fin.readline())
