ce8881840f702c0513d00cbb9b434fd337603546
Michael Doyle <meyekkul@gmail.com>
2020-05-08 16:25:14 +0800

    update generators labs

--- 15_generators/15_02_divisible.py
+++ 15_generators/15_02_divisible.py
@@ -3,3 +3,10 @@ Create a Generator that loops over the given list and prints out only
 the items that are divisible by 1111.
 
 '''
+
+gen = (x for x in range(1, 101) if x % 3 == 0)
+
+print(gen)
+
+for g in gen:
+    print(g)
--- 15_generators/15_03_floor.py
+++ 15_generators/15_03_floor.py
@@ -4,3 +4,5 @@ Adapt your Generator expression from the previous Exercise
 What numbers do you get?
 
 '''
+
+
