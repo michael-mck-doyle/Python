ab5f93b247f8e4bfde77a68c600eefbe92ea3918
Michael Doyle <meyekkul@gmail.com>
2020-05-10 10:01:52 +0800

    adding folder for Regex exercises

---
+++ 21_regex/regex_01.py
@@ -0,0 +1,17 @@
+'''
+
+__References__
+
+Regular Expressions: Regexes in Python, Part 1 - https://realpython.com/regex-python/
+
+regex has many functions available in the module "re" which are accessible
+by using "import re" or more specifically "from re import module_name"
+
+example function:
+re.search(<regex>, <string>)
+'''
+
+from re import search
+
+#search = (<regex>, <string>)
+
