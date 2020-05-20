3bee8f9d1218f172c8cab345a1c4d497094fd745
Michael Doyle <meyekkul@gmail.com>
2020-05-20 20:08:33 +0800

    adding SQLAlchemy to mysql connection files

---
+++ 23_databases/database_practice/database_connector_1.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+import pymysql
+from pprint import pprint
+
+'''
+engine = sqlalchemy.create_engine('mysql+pymysql://root:fidessa01@localhost/sakila')
+
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+print(actor.columns.keys())
+print(repr(metadata.tables['actor']))
+connection = engine.drop
+'''
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:fidessa01@localhost/sakila')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+
+query = sqlalchemy.select([actor])
+result_proxy = connection.execute(query)
+
+result_set = result_proxy.fetchall()
+pprint(result_set)
