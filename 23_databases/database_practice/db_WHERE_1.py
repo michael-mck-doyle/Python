b6ff55bba5a4e2ffefa786e188c1f84e053cb399
Michael Doyle <meyekkul@gmail.com>
2020-05-21 16:50:51 +0800

    updating database connector file

---
+++ 23_databases/database_practice/db_WHERE_1.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+import pymysql
+from pprint import pprint
+
+# run a select statement on a db table and introduce the "WHERE" filter
+engine = sqlalchemy.create_engine('mysql+pymysql://root:fidessa01@localhost/sakila')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+
+query = sqlalchemy.select([actor]).where(actor.columns.first_name == 'PENELOPE')
+#query = sqlalchemy.select([actor])
+result_proxy = connection.execute(query)
+
+result_set = result_proxy.fetchall()
+#result_set = result_proxy.fetchmany(5)
+pprint(result_set)
