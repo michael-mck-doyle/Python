d8b5b35320efc8358a4ce7fafc266169b24a87de
Michael Doyle <meyekkul@gmail.com>
2020-06-01 10:06:00 +0800

    updating API labs

--- 22_apis/Exercise_04.py
+++ 22_apis/Exercise_04.py
@@ -4,3 +4,30 @@ Write a program that makes a PUT request to update your user information to a ne
 Again make a GET request to confirm that your information has been updated.
 
 '''
+import requests
+from pprint import pprint
+
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+body = {
+    "id": 142,
+    "first_name": "Jemima",
+    "last_name": "Bond",
+    "email": "JB@email.com"
+}
+
+# here we execute the PUT request
+response = requests.put(base_url, json=body)
+print(f"Response Code: {response.status_code}")
+
+# GET request to retrieve the new data from the server
+key_value_params = {
+    "email": "JB@email.com"  # returns user based on email address
+
+}
+
+response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users", params=key_value_params)
+pprint(response.status_code)
+pprint(f"Header: {response.headers}")
+pprint(f"Body: {response.content}")
--- 22_apis/Exercise_05.py
+++ 22_apis/Exercise_05.py
@@ -4,3 +4,21 @@ Write a program that makes a DELETE request to remove the user your create in a 
 Again, make a GET request to confirm that information has been deleted.
 
 '''
+
+import requests
+from pprint import pprint
+
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+response = requests.delete(base_url + "/141")
+print(response.status_code)
+
+
+params = {
+    "id": "142"
+}
+
+response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users", params=params)
+pprint(response.status_code)
+pprint(f"Header: {response.headers}")
+pprint(f"Body: {response.content}")
