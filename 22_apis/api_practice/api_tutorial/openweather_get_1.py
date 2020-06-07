98ba59afaa42e0354ad38bf55b3f0cfc8e2e6792
Michael Doyle <meyekkul@gmail.com>
2020-06-07 17:47:56 +0800

    update api practice files

--- 22_apis/api_practice/api_tutorial/openweather_get_1.py
+++ 22_apis/api_practice/api_tutorial/openweather_get_1.py
@@ -3,13 +3,13 @@ from pprint import pprint
 
 
 key_value_params = {
-    "zip": "Ca$HMonEY@gmail.com", #returns user based on email address
+    "zip": 90250, #returns user based on email address
     "units": "imperial",
     "appid": "b67bc3439ff0f7f7ff6d0abc779d0bd8"
 
 }
 
-response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users", params=key_value_params)
+response = requests.get(" https://api.openweathermap.org/data/2.5/weather", params=key_value_params)
 pprint(response.status_code)
 pprint(f"Header: {response.headers}")
 pprint(f"Body: {response.content}")
