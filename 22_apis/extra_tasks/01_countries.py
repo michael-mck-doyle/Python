6edf813bb56bb0f6216762430fa0e5f0d54d3609
Michael Doyle <meyekkul@gmail.com>
2020-06-13 14:44:47 +0800

    updating api labs

--- 22_apis/extra_tasks/01_countries.py
+++ 22_apis/extra_tasks/01_countries.py
@@ -6,15 +6,28 @@ In your python program, parse and compare the data of the two responses:
 * Which country has the larger population?
 * How much does the area of the two countries differ?
 * Print the native name of both countries, as well as their capitals
+https://restcountries.eu/rest/v2/{service}?fields={field};{field};{field}
+https://restcountries.eu/rest/v2/all?fields=name;capital;currencies
 
 '''
+
 import requests
 from pprint import pprint
 
-base = "https://restcountries.eu/rest/v2/name/philippines"
+print()
+print("Action Menu:")
 
+country = input("Please enter the name of a country: ")
+param_1 = input("PLease enter a filter: ")
 
-response = requests.get(base)
+#params = {
+ #   "field1": param_1
+#}
+
+base = "https://restcountries.eu/rest/v2/name/"
+#params
+
+response = requests.get(base+country, params=param_1)
 
 pprint(response.status_code)
 pprint(response.encoding)
@@ -25,5 +38,8 @@ pprint(response.headers)
 # size (area)
 # native name
 # capital
-# 1. extract the information from the two countries and print to screen
-# 2. do a comparison of the aforementioned data features and print the results
+# 1. ask user to input the name of two countries they would like to do a comparison of
+# 2. the countries will be parameters in the method get requests
+# 3. GET individual information for a country or request all of the data and then parse it
+# 4. extract the information from the two countries and print to screen
+# 5. do a comparison of the aforementioned data features and print the results
